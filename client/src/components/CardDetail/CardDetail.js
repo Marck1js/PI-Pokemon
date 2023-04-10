5818dbeef982886084c219dc32a9161962a79b02
Marck1js <marckk63prog@gmail.com>
2023-04-10 15:24:08 -0300

    MJ: improve DetailPage

--- client/src/components/CardDetail/CardDetail.js
+++ client/src/components/CardDetail/CardDetail.js
@@ -1,21 +1,39 @@
 import React from "react";
 import Estilos from './CardDetail.module.css';
 
-export default  function CardDetail ({name,image,type,id,life,strength,defense,speed,height,weight}) {
-   
-   return (
-       <div  className={Estilos.contenedor}>
-           <h1 className={Estilos.name}>{name}</h1>
-          <div className={Estilos.divImagen}><img className={Estilos.image} src={image} width='100px' height='100px' alt="pokemon"/></div> 
-           <div className={Estilos.divType}>{type.map(e => <h1 className={Estilos.type} key={e}>Tipo: {e}</h1>)}</div>
-           <h1 className={Estilos.id}>Pokemon</h1>
-           <div className={Estilos.stats}><h1 className={Estilos.life}>vida: {life}</h1>
-           <h1 className={Estilos.strength}>fuerza: {strength}</h1>
-           <h1 className={Estilos.defense}>defensa: {defense}</h1>
-           <h1 className={Estilos.speed}>velocidad: {speed}</h1>
-           <h1 className={Estilos.height}>altura: {height}</h1>
-           <h1 className={Estilos.weight}>peso: {weight}</h1></div>
-       </div>
-   ) 
+export default function CardDetail({ name, image, type, id, life, strength, defense, speed, height, weight }) {
+
+    return (
+        <div className={Estilos.contenedor}>
+
+            <div className={Estilos.start}>
+                <p className={Estilos.name}>{name}</p>
+            </div>
+
+
+            <div className={Estilos.middle}>
+
+
+
+                <div className={Estilos.divImagen}>
+                    <img className={Estilos.image} src={image} alt="pokemon" />
+                </div>
+
+                <div className={Estilos.divType}>
+                    {type.map(e => <p className={Estilos.type} key={e}>Tipo: {e}</p>)}
+                </div>
+
+                <div className={Estilos.stats}>
+                    <p className={Estilos.life}>vida: {life}</p>
+                    <p className={Estilos.strength}>fuerza: {strength}</p>
+                    <p className={Estilos.defense}>defensa: {defense}</p>
+                    <p className={Estilos.speed}>velocidad: {speed}</p>
+                    <p className={Estilos.height}>altura: {height}</p>
+                    <p className={Estilos.weight}>peso: {weight}</p>
+                </div>
+            </div>
+
+        </div>
+    )
 
 }
